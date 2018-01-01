1d23e48686033ff811eee454079bb18e1a828d41
jogiya <yijunzou.stat@gmail.com>
2018-01-01 02:12:56 -0800

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,7 +1,10 @@
 electric <- read.table("household_power_consumption.txt", header = TRUE, sep = ";")
 data <- subset(electric, Date=="1/2/2007" | Date == "2/2/2007")
 head(data)
+
 Global_active_power <- as.numeric(as.character(data$Global_active_power))
+
 hist(Global_active_power, main = "Global Active Power", xlab = "Global Active Power(kilowatts)", col = "red")
+
 dev.copy(png, file = "plot1.png", height = 480, width = 480)
 dev.off()
